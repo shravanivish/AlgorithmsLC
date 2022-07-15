a5569367405d56ab3fc6c5e66b7f03e54b15c625
Shravani <79784238+shravanivish@users.noreply.github.com>
2022-07-15 16:41:47 +0530

    Initial commit

--- ContainsDuplicate.js
+++ ContainsDuplicate.js
@@ -12,7 +12,4 @@ Given an integer array nums, return true if any value appears at least twice in 
 // Input: nums = [1,1,1,3,3,4,3,2,4,2]
 // Output: true
 
-// Constraints:
-// 1 <= nums.length <= 105
-// -109 <= nums[i] <= 109
 
